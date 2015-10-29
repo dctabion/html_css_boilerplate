bfaaf6ec027250a99c5d24bb517297874e832891
Daniel Tabion <dctabion@gmail.com>
2015-10-29 16:23:19 -0500

    "updated boilerplate"

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,16 @@
 // hey hey hey
-alert('hey hey hey');
-window.onLoad(alert'hey)
+window.onload = function() {
+  // alert('hey hey');
+  statusReport('hey HEY HEY');
+
+  var username = prompt('What is your name?');
+  var mission = prompt('What is your quest?');
+
+  var contentContainer = document.getElementById('content');
+
+  contentContainer.innerHTML = username + ' is on a mission to ' + mission;
+}
+  function statusReport(message) {
+  console.log(message);
+  return message;
+}
